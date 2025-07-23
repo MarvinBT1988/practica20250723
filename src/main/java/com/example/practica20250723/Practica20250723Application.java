1378788ab4c1b33d77aa1d2a161dc14b8966ebca
Marvin Barrera <59856529+MarvinBT1988@users.noreply.github.com>
2025-07-23 14:30:31 +0000

    iniciar proyecto de sprinb boot

---
+++ src/main/java/com/example/practica20250723/Practica20250723Application.java
@@ -0,0 +1,13 @@
+package com.example.practica20250723;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica20250723Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica20250723Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practica20250723/Practica20250723ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica20250723;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica20250723ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
